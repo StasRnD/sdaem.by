bae4a01f5dc43c6f1ac570609ca19460fee67894
Stanislav <blackfur4a@mail.ru>
2022-11-16 23:49:38 +0300

    feat: creat newsDetails

--- src/components/routes/News/NewsDetails/NewsDetails.tsx
+++ src/components/routes/News/NewsDetails/NewsDetails.tsx
@@ -1,3 +1,17 @@
+import { useNewsDetailsProps } from './hooks';
+
 export const NewsDetails = () => {
-  return <div>Новость подробно</div>;
+  const { newsDetails, error, isLoading } = useNewsDetailsProps();
+
+  if (isLoading || !newsDetails) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <img src={newsDetails.image} />
+      <p>{newsDetails.text}</p>
+      <p>{newsDetails.title}</p>
+    </div>
+  );
 };
--- src/components/routes/News/NewsDetails/hooks.ts
+++ src/components/routes/News/NewsDetails/hooks.ts
@@ -1,1 +1,23 @@
-export function useNewsDetailsProps() {}
+import { useQuery } from 'react-query';
+import axios from 'axios';
+import { NewsDetails } from '../../../../types/types';
+import { useParams } from 'react-router-dom';
+
+export function useNewsDetailsProps() {
+  const { newsId } = useParams();
+
+  const { isLoading, error, data } = useQuery<NewsDetails>(
+    'newsDetails',
+    () => {
+      return axios
+        .get(`http://localhost:3000/news/${newsId}`)
+        .then((response) => response.data);
+    }
+  );
+
+  return {
+    isLoading,
+    error,
+    newsDetails: data,
+  };
+}
