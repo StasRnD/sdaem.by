a9781ffbe213d7febb04d34a01c479a74ac14f16
Stanislav <blackfur4a@mail.ru>
2022-11-23 22:38:29 +0300

    feat: add pagination news

--- src/components/routes/News/NewsList/NewsList.tsx
+++ src/components/routes/News/NewsList/NewsList.tsx
@@ -1,7 +1,7 @@
 import { NewsItem } from '../NewsItem/NewsItem';
 import { Pagination } from '../../Pagination/Pagination';
 import { NewsDetails } from '../../../../types/types';
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import set from 'lodash/set';
 
 type newsDetailsProps = {
@@ -14,19 +14,23 @@ interface FiltersProps {
 }
 
 export const NewsList = ({ newsList }: newsDetailsProps) => {
+  const url = new URL(document.location.href);
+
   const [filters, setFilters] = useState(() => {
-    const url = new URL(document.location.href);
     return {
       query: url.searchParams.get('query') || '',
     };
   });
 
-  const [searchValue, setSearchValue] = useState<FiltersProps['query']>(
-    filters.query
-  );
+  const [activePage, setActivePage] = useState(() => {
+    return Number(url.searchParams.get('page')) || 1;
+  });
+
+  const [searchValue, setSearchValue] = useState(filters.query);
+
+  const itemsPerPage = 9;
 
   const onChange: FiltersProps['onChange'] = (value, filterName) => {
-    const url = new URL(document.location.href);
     setFilters((oldFilters) => set({ ...oldFilters }, filterName, value));
     url.searchParams.set(filterName, value);
     window.history.pushState(null, '', url.search);
@@ -34,6 +38,8 @@ export const NewsList = ({ newsList }: newsDetailsProps) => {
 
   const onSubmitFilterForm: React.FormEventHandler<HTMLFormElement> = (evt) => {
     evt.preventDefault();
+    setActivePage(1);
+    url.searchParams.delete('page');
     onChange(searchValue, 'query');
   };
 
@@ -43,11 +49,26 @@ export const NewsList = ({ newsList }: newsDetailsProps) => {
     setSearchValue(evt.target.value);
   };
 
-  const filterNews = newsList.filter((newsItem) =>
-    (newsItem.title + newsItem.text)
-      .toLowerCase()
-      .includes(filters.query.toLowerCase())
-  );
+  const onClickButtonPagination = (page: number) => {
+    url.searchParams.set('page', String(page));
+    window.history.pushState(null, '', url.search);
+    setActivePage(page);
+  };
+
+  const filterNews = useMemo(() => {
+    return newsList.filter((newsItem) =>
+      (newsItem.title + newsItem.text)
+        .toLowerCase()
+        .includes(filters.query.toLowerCase())
+    );
+  }, [filters, newsList]);
+
+  const paginatedNews = useMemo(() => {
+    const indexOfLastNews = activePage * itemsPerPage;
+    const indexOfFirstNews = indexOfLastNews - itemsPerPage;
+
+    return filterNews.slice(indexOfFirstNews, indexOfLastNews);
+  }, [activePage, filterNews]);
 
   return (
     <section className='newsList news__newsList'>
@@ -65,10 +86,15 @@ export const NewsList = ({ newsList }: newsDetailsProps) => {
         <button type='submit' className='newsList__button'></button>
       </form>
 
-      {filterNews.slice(0, 10).map((newsItem: NewsDetails) => {
+      {paginatedNews.map((newsItem: NewsDetails) => {
         return <NewsItem key={newsItem.id} data={newsItem} />;
       })}
-      <Pagination />
+      <Pagination
+        pageQuantity={filterNews}
+        activePage={activePage}
+        onClickButtonPagination={onClickButtonPagination}
+        itemsPerPage={itemsPerPage}
+      />
     </section>
   );
 };
--- src/components/routes/Pagination/Pagination.tsx
+++ src/components/routes/Pagination/Pagination.tsx
@@ -1,3 +1,39 @@
-export const Pagination = () => {
-  return <div>Кнопки</div>;
+import { NewsDetails } from '../../../types/types';
+
+type paginationProps = {
+  pageQuantity: NewsDetails[];
+  activePage: number;
+  itemsPerPage: number;
+  onClickButtonPagination: (page: number) => void;
+};
+
+export const Pagination = ({
+  pageQuantity,
+  activePage,
+  onClickButtonPagination,
+  itemsPerPage,
+}: paginationProps) => {
+  const quantityButtons = Array.from(
+    { length: Math.ceil(pageQuantity.length / itemsPerPage) },
+    (v, i) => i + 1
+  );
+
+  return (
+    <section>
+      {pageQuantity.length > itemsPerPage &&
+        quantityButtons.map((buttonValue, index) => {
+          return (
+            <button
+              key={index}
+              onClick={() => {
+                onClickButtonPagination(buttonValue);
+              }}
+              className={`${activePage === buttonValue && 'activeButton'}`}
+            >
+              {buttonValue}
+            </button>
+          );
+        })}
+    </section>
+  );
 };
